02f96120981448ce98fccc0ffe72ec58c8d408f6
Lavika Makwana <lmakwana@isystango.com>
2020-01-09 16:54:55 +0530

    changed UI and mod_message to message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,10 +17,6 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
-
-
-
 scroll_bottom = function() {
   if ($('#messages').length > 0) {
     $('#messages').scrollTop($('#messages')[0].scrollHeight);
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
     message = current_user.messages.build(message_params)
     if message.save
       ActionCable.server.broadcast "chatroom_channel",
-                                    mod_message: message_render(message)
+                                    message: message_render(message)
     end
   end
 
